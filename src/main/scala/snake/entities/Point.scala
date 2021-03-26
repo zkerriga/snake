d2185e0ad5948c2ae87a6546553a257b0f65b8ec
zkerriga <daniil.63@yandex.ru>
2021-03-27 02:53:05 +0300

    Distance fixed

--- src/main/scala/snake/entities/Point.scala
+++ src/main/scala/snake/entities/Point.scala
@@ -3,7 +3,6 @@ package snake.entities
 case class Point(x: Int, y: Int)
 
 object Point {
-  def distanceSquare(p1: Point, p2: Point): Double = {
-    (p1.x - p2.x) ^ 2 + (p1.y - p2.y) ^ 2
-  }
+  def distanceSquare(p1: Point, p2: Point): Double =
+    scala.math.hypot(p2.x - p1.x, p2.y - p2.y)
 }
