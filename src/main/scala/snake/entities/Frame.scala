1e928988e4a020b365e4bb0d4fc2821dd4dae613
zkerriga <daniil.63@yandex.ru>
2021-03-26 23:26:01 +0300

    Snake improve

--- src/main/scala/snake/entities/Frame.scala
+++ src/main/scala/snake/entities/Frame.scala
@@ -1,14 +1,9 @@
 package snake.entities
 
 case class Frame(min: Point, max: Point) {
-  def points: Seq[Point] = {
-    for (i <- min.x until max.x + 1;
-         j <- min.y until max.y + 1
-         if i == min.x ||
-           i == max.x ||
-           j == min.y ||
-           j == max.y)
-      yield Point(i, j)
-  }
+  def points: Seq[Point] = for {
+    i <- min.x to max.x
+    j <- min.y to max.y
+    if i == min.x || i == max.x || j == min.y || j == max.y
+  } yield Point(i, j) /* todo: remove this borders */
 }
-
--- src/main/scala/snake/entities/Game.scala
+++ src/main/scala/snake/entities/Game.scala
@@ -1,27 +1,28 @@
 package snake.entities
 
 case class Game(food: Food, snake: Snake, frame: Frame, elapsedTime: Float, start: Snake) {
-  val framePoints: List[Point] = frame.points.toList
-
-  def handle(input: List[Direction]): Game = {
+  def handle(input: Seq[Direction]): Game = {
     if (input.isEmpty) {
       this
-    } else {
-      copy(snake = input.foldLeft(snake)((s, d) => s.turn(d)))
+    }
+    else {
+      copy(snake = input.foldLeft(snake)(
+        (snake, direction) => snake.turn(direction)
+      ))
     }
   }
 
   def update(deltaTime: Float): Game = {
     val elapsed = elapsedTime + deltaTime
-    if (elapsed > 0.1) {
-      val game = copy(snake = snake.move(), elapsedTime = 0)
-      if (!game.snake.headIsIn(frame)) {
+    if (elapsed > 0.06) {
+      val game = copy(snake = snake.move, elapsedTime = 0)
+      if (game.snake.isHeadbutt(frame) || game.snake.isBitTail) {
         game.reset()
-      } else if (game.snake.isBitTail()) {
-        game.reset()
-      } else if (game.snake.canEat(food)) {
+      }
+      else if (game.snake.canEat(food)) {
         game.copy(snake = snake.eat(food), food = food.moveRandomIn(frame))
-      } else {
+      }
+      else {
         game
       }
     } else {
@@ -31,7 +32,5 @@ case class Game(food: Food, snake: Snake, frame: Frame, elapsedTime: Float, star
 
   def reset(): Game = copy(snake = start)
 
-  def points: List[Point] = {
-    (food.body :: snake.body) ::: framePoints
-  }
+  def points: Seq[Point] = food.body +: snake.body ++: frame.points
 }
